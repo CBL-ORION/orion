d987b6b4b0cd96647e5c2eb3758e277e9660939a
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-06-29 13:10:01 -0500

    ndarray3 data structure

---
+++ ndarray3.c
@@ -0,0 +1,32 @@
+#include "ndarray3.h"
+
+ndarray3* ndarray3_wrap( const float* p, const size_t sz_x, const size_t sz_y, const size_t sz_z ) {
+	ndarray3* n = (ndarray3*)malloc( sizeof(ndarray3) );
+	n->wrap = 1;
+
+	n->sz[0] = sz_x;
+	n->sz[1] = sz_y;
+	n->sz[2] = sz_z;
+
+	return n;
+}
+
+ndarray3* ndarray3_create(const size_t sz_x, const size_t sz_y, const size_t sz_z) {
+	ndarray3* n = (ndarray3*)malloc( sizeof(ndarray3) );
+
+	n->p = (float*) malloc( sizeof(float) * sz_x * sz_y * sz_z );
+	n->wrap = 0;
+
+	n->sz[0] = sz_x;
+	n->sz[1] = sz_y;
+	n->sz[2] = sz_z;
+
+	return n;
+}
+
+void ndarray3_destroy( ndarray3* n ) {
+	if( ! n->wrap ) free( n->p );
+	free( n );
+}
+
+
