d0a301a8ff904e8e0b7837f8bd0a9745e869aa62
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-06-30 14:46:17 -0500

    add tests

---
+++ lib/t/canary.c
@@ -0,0 +1,13 @@
+ #include <stddef.h>
+ #include <tap/basic.h>
+
+ int main(void) {
+     plan(2);
+
+     ok(1, "the first test");
+     is_int(42, 42, NULL);
+     diag("a diagnostic, ignored by the harness");
+     /*skip("a skipped test");*/
+
+     return 0;
+ }
---
+++ lib/t/ndarray/ndarray.c
@@ -0,0 +1,12 @@
+#include <stddef.h>
+#include <tap/basic.h>
+
+#include "ndarray/ndarray3.h"
+
+int main(void) {
+	plan(1);
+
+	ok(1, "the first test");
+
+	return 0;
+}
