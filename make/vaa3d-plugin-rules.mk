4f53a063b2637e733f818c68934e3c397ccd23ff
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-07-02 15:23:19 -0500

    make sure that the Vaa3D plugin takes its VAA3DPATH variable from Makefile
    By commenting out the variable in the .pro file, VAA3DPATH must be set
    from the `qmake` command line.

--- make/vaa3d-plugin-rules.mk
+++ make/vaa3d-plugin-rules.mk
@@ -2,5 +2,8 @@ vaa3d-plugin-orion-matlab: ${ORION_MATLAB_LIB_OBJ}
 
 $(ORION_MATLAB_LIB_OBJ): ${ORION_MATLAB_LIB_SRC_PATH}/orion_matlab_plugin.cpp
 	mkdir -p $(ORION_MATLAB_LIB_BUILD_PATH)
-	qmake  -makefile -o $(ORION_MATLAB_LIB_BUILD_PATH)/Makefile VAA3DPATH=${VAA3D_PATH} $(ORION_MATLAB_LIB_SRC_PATH)/orion_matlab.pro
+	qmake  -makefile \
+		-o $(ORION_MATLAB_LIB_BUILD_PATH)/Makefile \
+		VAA3DPATH=${FULL_VAA3D_PATH} \
+		$(ORION_MATLAB_LIB_SRC_PATH)/orion_matlab.pro
 	$(MAKE) -C$(ORION_MATLAB_LIB_BUILD_PATH)
