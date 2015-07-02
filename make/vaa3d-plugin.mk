f09f4fda59fdb31df8c8f1b23347777120c146f2
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-07-02 15:18:45 -0500

    add make variable ORION_MATLAB_LIB_BUILD_PATH
    This indicates where qmake will work from.

--- make/vaa3d-plugin.mk
+++ make/vaa3d-plugin.mk
@@ -1,3 +1,4 @@
 ## Targets
 ORION_MATLAB_LIB_OBJ := ${VAA3D_PATH}/bin/plugins/bigneuronhackathon/orion_matlab/liborion_matlab.so
 ORION_MATLAB_LIB_SRC_PATH := $(LIBDIR)/vaa3d-plugin/orion-matlab
+ORION_MATLAB_LIB_BUILD_PATH := $(BUILDDIR)/vaa3d-plugin/orion-matlab
