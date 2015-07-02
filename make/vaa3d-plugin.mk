089597ebfb69c1365cbc38165535767d7c1207c1
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-07-02 17:42:13 -0500

    add a note about DESTDIR for orion_matlab plugin

--- make/vaa3d-plugin.mk
+++ make/vaa3d-plugin.mk
@@ -1,4 +1,9 @@
 ## Targets
-ORION_MATLAB_LIB_OBJ := ${VAA3D_PATH}/bin/plugins/bigneuronhackathon/orion_matlab/liborion_matlab.so
+
+# if this is changed, then the DESTDIR variable in lib/vaa3d-plugin/orion-matlab/orion_matlab.pro
+# must also be changed
+ORION_MATLAB_LIB_OBJ_DIR := ${VAA3D_PATH}/bin/plugins/bigneuronhackathon/orion_matlab
+
+ORION_MATLAB_LIB_OBJ := $(ORION_MATLAB_LIB_OBJ_DIR)/liborion_matlab.so
 ORION_MATLAB_LIB_SRC_PATH := $(LIBDIR)/vaa3d-plugin/orion-matlab
 ORION_MATLAB_LIB_BUILD_PATH := $(BUILDDIR)/vaa3d-plugin/orion-matlab
