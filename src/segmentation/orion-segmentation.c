4cde6d79a96fd3e2686fd44e0cc70a1b1848c404
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-07-25 21:35:59 -0500

    initial skeleton of scale reading code

--- src/segmentation/orion-segmentation.c
+++ src/segmentation/orion-segmentation.c
@@ -3,10 +3,30 @@
 
 #include "simple-log/simplelog.h"
 
+#include "container/array.h"
+
+/*
+  if( strcmp( argv[i], "--scale" ) == 0 ) {
+	array_insert_float( scales, atof( argv[i + 1] ) );
+  } else if( strcmp( argv[i], "--input" ) == 0 ) {
+	WARN_UNIMPLEMENTED
+  } else if(  strcmp( argv[i], "--output" ) == 0 ) {
+	WARN_UNIMPLEMENTED
+  }
+*/
+
 int main( int argc, char * argv[] ) {
 	LOG_INFO("Starting %s", argv[0] );
 
+	/*
+		--scale 1.0
+		--scale 2.0
+		--scale 3.0
+		--input  filename.mhd
+		--output filename.mhd
+	 */
 
+	array_float* scales = array_new_float( 10 );
 
 	LOG_INFO("Stopping %s", argv[0] );
 	return EXIT_SUCCESS;
