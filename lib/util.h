3d315f914b0374a7f76c26f4c7fac88a6aca40d1
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-07-25 21:35:02 -0500

    add a macro to warn of unimplemented code

--- lib/util.h
+++ lib/util.h
@@ -1,6 +1,24 @@
 #ifndef UTIL_H
 #define UTIL_H 1
 
+#include <stdlib.h>
+
 #define SQUARED(x) ( (x) * (x) )
+
+#define STRINGIZE_HELPER(x) #x
+#define STRINGIZE(x) STRINGIZE_HELPER(x)
+
+#define SHOWORIGIN           __FILE__ "(" STRINGIZE(__LINE__) "): "
+
+/* TODO test on MSVC? */
+#define DO_PRAGMA(x) _Pragma (#x)
+#define TODO(x) DO_PRAGMA(message ("TODO - " #x))
+
+/*#define WARN_UNIMPLEMENTED DO_PRAGMA(message ("Warning: unimplemented")) */
+#define WARN_UNIMPLEMENTED \
+	 do { \
+		fprintf(stderr, "%s Warning: unimplemented in %s\n", SHOWORIGIN, __FUNCTION__); \
+		exit(EXIT_FAILURE); \
+	} while(0)
 
 #endif /* UTIL_H */
