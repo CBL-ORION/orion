5aad05e31b42119ebb6294c340c629b50e4dfd4b
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-07-25 22:33:39 -0500

    shorten the type in array_impl

--- lib/container/array_impl.h
+++ lib/container/array_impl.h
@@ -13,15 +13,18 @@ typedef struct {
 	TYPE *data;
 } TYPED_NAME(array);
 
-TYPED_NAME(array) * TYPED_NAME(array_new) ( size_t size ) {
-#define _T  TYPED_NAME(array)
+#define _T  TYPED_NAME(array) /* shorten type */
+
+_T* TYPED_NAME(array_new)( size_t size ) {
+
 	_T* arr = malloc( sizeof( _T ) );
 
 	WARN_UNIMPLEMENTED;
 	return NULL;
-#undef _T
 }
 
-void TYPED_NAME(array_insert) ( TYPED_NAME(array) *array, TYPE data ) {
+void TYPED_NAME(array_insert) ( _T* array, TYPE data ) {
 	WARN_UNIMPLEMENTED;
 }
+
+#undef _T
