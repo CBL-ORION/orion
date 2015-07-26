28e08ce1dca38388a56f136d13b69c813633162c
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-07-26 04:25:16 -0500

    remove more stray spaces

--- hdaf.m
+++ hdaf.m
@@ -1,14 +1,14 @@
-function val=hdaf(n,sigma,x)  
+function val=hdaf(n,sigma,x)
   en=1.0;
   ft=zeros(n,1);
    ft(1)=1.0;
-   if n > 1 
+   if n > 1
     for i=2:n
       ft(i)=ft(i-1)*i;
     end
    end
 
-  
+
   for i=1:n
     en=en+((x*sigma^2)/2).^i/ft(i);
   end
@@ -16,5 +16,4 @@ function val=hdaf(n,sigma,x)
   val=(en.*exp((-1.0)*x*(sigma^2)/2));
 
 
-  
-    
+
