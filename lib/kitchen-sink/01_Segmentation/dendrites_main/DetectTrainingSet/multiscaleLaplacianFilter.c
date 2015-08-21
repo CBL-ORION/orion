bd534f8fd57e7aa46f3d672f75ab5397d9da0a91
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-08-20 22:59:24 -0500

    quick-and-dirty creation of C files that match every existing MATLAB file
    This just creates empty files by running the following commands:
    
        find external/orion3mat/ -iname "*.m" -type f -exec dirname {} \; | grep  "/0[123]" | grep -v -e "frangi_filter_version2a" -e "libsvm-3.16"  | uniq | sed 's|external/orion3mat|lib/kitchen-sink|' | xargs mkdir -p
        find external/orion3mat/ -iname "*.m" -type f  | grep  "/0[123]" | grep -v -e "frangi_filter_version2a" -e "libsvm-3.16"  | uniq | sed 's|external/orion3mat|lib/kitchen-sink|' | sed 's|.m$|.c|' | xargs touch

---
+++ lib/kitchen-sink/01_Segmentation/dendrites_main/DetectTrainingSet/multiscaleLaplacianFilter.c
@@ -0,0 +1,8 @@
+/* TODO
+ *
+ * Refactor: multiscaleLaplacianFilter
+ */
+
+void [multiscaleLaplacian scales] = orion_multiscaleLaplacianFilter( ndarray3* input_volum ,Lap,p) {
+	WARN_UNIMPLEMENTED;
+}
---
+++ lib/kitchen-sink/01_Segmentation/dendrites_main/DetectTrainingSet/readNegativeSamples.c
@@ -0,0 +1,17 @@
+
+#include <stdbool.h>
+#include "container/vector.h"
+#include "ndarray/ndarray3.h"
+
+struct {
+	bool multiscale;
+	vector_float spacing;
+} orion_readNegativeSamples_param;
+
+/* TODO
+ *
+ * Refactor: readNegativeSamples
+ */
+void orion_readNegativeSamples( ndarray3* input_volume,  ) {
+	WARN_UNIMPLEMENTED;
+}
