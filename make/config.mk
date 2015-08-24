e6764331aa5d1a7c86fa1794afb7c904f1142240
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-08-24 14:07:14 -0500

    remove ASAN flag

--- make/config.mk
+++ make/config.mk
@@ -2,7 +2,7 @@ include make/platform.mk
 
 CPPFLAGS += -Ilib
 CFLAGS   += -std=c99 -O2
-CFLAGS   += -fsanitize=address
+#CFLAGS   += -fsanitize=address
 #CFLAGS   += -Wall
 #CFLAGS   := -std=c11
 LDFLAGS  += -lm $(CPPFLAGS)
