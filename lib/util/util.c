c2bc0e90163c8880e13f729270f05ac2998f823d
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-08-31 12:39:02 -0500

    add a check for the errno when using die()

--- lib/util/util.c
+++ lib/util/util.c
@@ -1,12 +1,18 @@
 #include "util/util.h"
 
 #include <string.h>
+#include <errno.h>
 
 void _real_die(const char* msg_fmt, const char* origin, ...) {
 	va_list args;
 	va_start(args, origin); \
 	fprintf(stderr, "%s ", origin);
 	vfprintf(stderr, msg_fmt, args);
+	if(errno) {
+		/* TODO use strerror_r for thread-safe */
+		fprintf(stderr, ": %s", strerror(errno));
+		errno = 0; /* clear */
+	}
 	fprintf(stderr, "\n");
 	va_end(args);
 	exit(EXIT_FAILURE);
