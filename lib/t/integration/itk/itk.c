f7e7f12c54a774d9d3c66c44d65c6b33a8e304d3
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-09-02 09:58:27 -0500

    make the ITK integration tests TODO for now

--- lib/t/integration/itk/itk.c
+++ lib/t/integration/itk/itk.c
@@ -5,11 +5,19 @@
 #include "io/format/mhd.h"
 
 int main(void) {
-	plan(2);
+	plan( 1 + 1 );
 
+	skip("rest of tests can not run without MetaInfo file reading");
+	return EXIT_SUCCESS;
+
+/* 1 test */
 	ndarray3* n = orion_read_mhd("test-data/DIADEM/NPF/NPF023/NPF023.mhd");
+	ok( NULL != n, "# todo reading MetaInfo file is not yet finished");
 
+/* 1 test */
 	array_ndarray3* frangi = orion_filter_method_frangi(n, 3.0);
+	ok( NULL != frangi, "Frangi filter returns non-NULL result" );
+	TODO( test that the length of the frangi array is 3 );
 	/* TODO */
 
 	return EXIT_SUCCESS;
