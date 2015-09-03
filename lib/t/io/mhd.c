1af4ad8a78e85761f06f0be20f9d1c15cbd32a1a
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-09-02 21:37:56 -0500

    MetaInfo format: add more tests to check size of ndarray

--- lib/t/io/mhd.c
+++ lib/t/io/mhd.c
@@ -6,18 +6,31 @@
 #include "io/format/mhd.h"
 
 int main(void) {
-	plan(3);
+	plan(3 + 4);
 
 	char* mhd_file = "test-data/DIADEM/NPF/NPF023/NPF023.mhd";
-	orion_mhd_metadata* meta = orion_read_mhd_metdata( mhd_file );
 
-	is_string( "NPF023.raw", meta->ElementDataFile, "the data file is right" );
-	is_int(3, meta->NDims, "is a 3D volume" );
-	is_int(3, array_length_int(meta->DimSize), "DimSize is of length 3" );
+	{
+		/* 3 tests */
+		orion_mhd_metadata* meta = orion_read_mhd_metdata( mhd_file );
 
-	/*ndarray3* vol = orion_read_mhd(  );*/
+		is_string( "NPF023.raw", meta->ElementDataFile, "the data file is right" );
+		is_int(3, meta->NDims, "is a 3D volume" );
+		is_int(3, array_length_int(meta->DimSize), "DimSize is of length 3" );
+	}
 
-	/* TODO check the size */
+	{
+		/* 4 tests */
+		ndarray3* vol = orion_read_mhd( mhd_file );
+		ok( NULL != vol, "reading the MetaInfo file gives a non-NULL ndarray");
+
+		/* check the size */
+		is_int( 1024, vol->sz[0], "1st dim is correct size");
+		is_int( 1024, vol->sz[1], "2nd dim is correct size");
+		is_int(   57, vol->sz[2], "3rd dim is correct size");
+	}
+
+
 
 
 	return EXIT_SUCCESS;
