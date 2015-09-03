a36fb340ac6f767bf4b2dc394e3629a3ceb9634b
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-09-02 19:32:50 -0500

    document the orion_filepath struct

--- lib/io/path/path.h
+++ lib/io/path/path.h
@@ -6,11 +6,26 @@
 #include "container/array.h"
 
 typedef struct {
+	/* A list of components of the path.
+	 *
+	 * May either refer to
+	 *   - an absolute path (if has_root_component == true) or
+	 *   - a relative path (if has_root_component == false).
+	 */
 	array_str* components;
 
 	bool has_root_component;
+	/* root part of the path:
+	 * - On Unix: "/"
+	 * - On Windows: C:/, D:/, etc.
+	 *
+	 * This is only used if has_root_component == true.
+	 *
+	 * Must end in in the file separator ("/").
+	 */
 	char* root_component;
 } orion_filepath;
+
 extern orion_filepath* orion_filepath_new_from_string(const char* fp_string);
 extern void orion_filepath_dump(orion_filepath* fp);
 extern char* orion_filepath_to_string(orion_filepath* fp);
