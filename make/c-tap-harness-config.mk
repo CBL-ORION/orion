d1c80d8daa79ffa87a9a6216fe3c62743053627d
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-09-03 10:57:18 -0500

    make: add `TEST_VERBOSE` option to pass `-v` flag to the TAP harness
    This allows for showing all the output for tests.

--- make/c-tap-harness-config.mk
+++ make/c-tap-harness-config.mk
@@ -1,8 +1,14 @@
 ## Tests
 C_TAP_HARNESS_PATH := external/c-tap-harness/c-tap-harness
 
+ifdef TEST_VERBOSE
+  TEST_VERBOSE_FLAG := -v
+else
+  TEST_VERBOSE_FLAG :=
+endif
+
 #RUNTESTS := $(C_TAP_HARNESS_PATH)/tests/runtest
-RUNTESTS := $(C_TAP_HARNESS_PATH)/tests/runtests
+RUNTESTS := $(C_TAP_HARNESS_PATH)/tests/runtests $(TEST_VERBOSE_FLAG)
 
 TEST_CFLAGS   :=
 TEST_CPPFLAGS := -I$(C_TAP_HARNESS_PATH) -I$(C_TAP_HARNESS_PATH)/tests
