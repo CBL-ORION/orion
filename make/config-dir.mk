fd09d4f9dc1cc87d7ba57b4065a7a34d85f04889
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-09-06 14:23:23 -0500

    further changes to the build configuration settings

--- make/config-dir.mk
+++ make/config-dir.mk
@@ -14,7 +14,10 @@ BUILDTESTDIR := $(BUILDDIR)/t
 # output used to build deps information for make(1)
 DEPDIR   := .deps
 
-OUTPUT_DIRS := $(BUILDDIR) $(DEPDIR) $(BINDIR)
+OUTPUT_DIRS := $(BUILDDIR) $(BUILDTESTDIR) $(DEPDIR) $(BINDIR)
 
 FULL_LIBDIR := `cd ${LIBDIR}; pwd`
 FULL_BUILDDIR := `cd ${BUILDDIR}; pwd`
+
+GCOVDIR := report-gcov
+LCOVDIR := report-lcov
--- make/config.mk
+++ make/config.mk
@@ -29,28 +29,52 @@ DEBUGFLAGS := -g
 CMAKEDEBUGFLAGS := -DCMAKE_BUILD_TYPE=debug
 
 # GCOVFLAGS: used when BUILD_GCOV variable exists
-GCOVFLAGS :=  -g -O0 -fprofile-arcs -ftest-coverage --coverage -fprofile-dir=$(BUILDDIR)
+GCOVFLAGS.PROFILEDIR = -fprofile-dir=`dirname $@`
+#GCOVFLAGS.PROFILEDIR =
+GCOVFLAGS =  -g -O0 -fprofile-arcs -ftest-coverage --coverage
 CMAKEGCOVFLAGS :=
+
+# GPROFFLAGS: used when the BUILD_GPROF variable exists
+GPROFFLAGS := -pg
+CMAKEGPROFLAGS :=
 
 # RELEASEFLAGS, CMAKERELEASEFLAGS: used when neither BUILD_DEBUG nor BUILD_GCOV exist
 RELEASEFLAGS :=
 CMAKERELEASEFLAGS :=
 
+ifdef BUILD_GCOV
+# using OPT flags when using gcov is not recommended
+BUILD_OPT_FLAGS := 0
+CFLAGS     += $(GCOVFLAGS)
+CXXFLAGS   += $(GCOVFLAGS)
+LDFLAGS    += $(GCOVFLAGS) $(GCOVFLAGS.PROFILEDIR)
+CMAKEFLAGS += $(CMAKEGCOVFLAGS)
+endif
+
+ifdef BUILD_GPROF
+CFLAGS     += $(GPROFFLAGS)
+CXXFLAGS   += $(GPROFFLAGS)
+LDFLAGS    += $(GPROFFLAGS)
+CMAKEFLAGS += $(CMAKEGPROFLAGS)
+endif
+
 ifdef BUILD_DEBUG
 CFLAGS     += $(DEBUGFLAGS)
 LDFLAGS    += $(DEBUGFLAGS)
 CMAKEFLAGS += $(CMAKEDEBUGFLAGS)
-else ifdef BUILD_GCOV
-#CFLAGS     += $(GCOVFLAGS)
-LDFLAGS    += $(GCOVFLAGS)
-CMAKEFLAGS += $(CMAKEGCOVFLAGS)
 else # BUILD_RELEASE
+# set BUILD_OPT_FLAGS only if it has not been unset before
+BUILD_OPT_FLAGS ?= 1
 CFLAGS     += $(RELEASEFLAGS)
-# add optimisation flags
-CFLAGS     += $(CFLAGS.OPT)
 LDFLAGS    += $(RELEASEFLAGS)
 CMAKEFLAGS += $(CMAKERELEASEFLAGS)
 endif
+
+ifeq ($(BUILD_OPT_FLAGS),1)
+# add optimisation flags
+CFLAGS     += $(CFLAGS.OPT)
+endif
+
 
 CMAKE := cmake
 ### Set variables to pass into CMakeLists.txt
@@ -59,6 +83,6 @@ CMAKEFLAGS := -D LIBDIR=${FULL_LIBDIR} -D BUILDDIR=${FULL_BUILDDIR}
 CMAKE.generate := $(CMAKE) $(CMAKEFLAGS)
 
 GCOV := gcov
-
+GPROF := gprof
 
 include make/dep.mk
