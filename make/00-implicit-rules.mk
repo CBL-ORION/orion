09975abbc896557b00949fecd7d1dbdec5ed453d
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-09-06 14:24:15 -0500

    when building with BUILD_GCOV, move the .gcno files to the output directory

--- make/00-implicit-rules.mk
+++ make/00-implicit-rules.mk
@@ -8,11 +8,19 @@ $(BUILDTESTDIR)/%$(EXEEXT): $(TESTDIR)/%.c
 	@$(MKDIR_DEPEND.c)
 	@$(MKDIR_BUILDTESTDIR)
 	$(LINK.c) $^ $(LOADLIBES) $(LDLIBS) -o $@
+ifdef BUILD_GCOV
+	# move the .gcno file to the directory with the executable
+	mv `basename $@ $(EXEEXT)`.gcno `dirname $@`
+endif
 
 $(BINDIR)/%$(EXEEXT): $(SRCDIR)/%.c
 	@$(MKDIR_DEPEND.c)
 	@$(MKDIR_BIN)
 	$(LINK.c) $^ $(LOADLIBES) $(LDLIBS) -o $@
+ifdef BUILD_GCOV
+	# move the .gcno file to the directory with the executable
+	mv `basename $@ $(EXEEXT)`.gcno `dirname $@`
+endif
 
 $(BINDIR)/%.o : $(SRCDIR)/%.c
 	@$(MKDIR_DEPEND.c)
