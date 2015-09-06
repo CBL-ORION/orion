313b0529d2cb4ff243a823f53a1e61dc1bed61ce
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-09-06 14:22:53 -0500

    add an order-only prereq on $(BUILDDIR)

--- make/itk-config.mk
+++ make/itk-config.mk
@@ -1,7 +1,8 @@
 ITK_CONFIG_MK := make/itk/itk-config.mk.gen
 include $(ITK_CONFIG_MK)
 
-$(ITK_CONFIG_MK):  make/itk/CMakeLists.txt
+$(ITK_CONFIG_MK):  make/itk/CMakeLists.txt \
+		| $(BUILDDIR)
 	@$(MKDIR_BUILD)
 	$(CMAKE.generate) -B$(BUILDDIR)/.make/itk -Hmake/itk
 	echo "ITK_LDLIBS += `./tool/script/extract-linker-flags.pl < $(BUILDDIR)/.make/itk/CMakeFiles/Pi.dir/link.txt`" >> $@
