cfd4dc7e2b8ae3b668299b2160d389f0f739d520
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-09-07 06:57:57 -0500

    ITK integration: free the data at the end of the test

--- lib/t/integration/itk/itk.c
+++ lib/t/integration/itk/itk.c
@@ -10,13 +10,21 @@ int main(void) {
 /* 1 test */
 	ndarray3* n = orion_read_mhd("test-data/ITK/HeadMRVolume/HeadMRVolume.mhd");
 	/*DEBUG*/ndarray3_dump( n );
-	ok( NULL != n, "read the metainfo data");
+	ok( NULL != n, "read in the raw data");
+
 
 /* 2 tests */
 	array_ndarray3* frangi = orion_filter_method_frangi(n, 3.0);
 	ok( NULL != frangi, "Frangi filter returns non-NULL result" );
 	is_int( 3, array_length_ndarray3(frangi),
 			"the length of the Frangi array is 3");
+
+
+	for( int idx = 0; idx < array_length_ndarray3(frangi); idx++ ) {
+		ndarray3_free( array_get_ndarray3(frangi, idx) );
+	}
+	array_free_ndarray3( frangi );
+	ndarray3_free(n);
 
 	return EXIT_SUCCESS;
 }
