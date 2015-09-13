ed160fae4748e7e809c273b87ddcc71d5484e8d4
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-09-12 22:22:05 -0500

    numeric: add M_PI constant def if it has not been defined yet

---
+++ lib/numeric/def.h
@@ -0,0 +1,11 @@
+#ifndef NUMERIC_DEF_H
+#define NUMERIC_DEF_H 1
+
+/* system headers */
+#include <math.h>
+
+#ifndef M_PI
+  #define M_PI   3.14159265358979323846   /* pi */
+#endif /* M_PI */
+
+#endif /* NUMERIC_DEF_H */
