b748c567b17c6b7fcb3e1ade117834f34ebaa9c0
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-10-12 23:43:38 -0500

    matlab_colonop: test: free all the float64* at the end

--- lib/t/numeric/func_matlab_colonop.c
+++ lib/t/numeric/func_matlab_colonop.c
@@ -11,25 +11,30 @@ int main(void) {
 		size_t n;
 		float64* vec = matlab_colonop_float64(0, 1, 3, &n);;
 		is_int( 4, n, "0:1:3 is of length 4");
+		free(vec);
 	}
 	{/* 1 tests */
 		size_t n;
 		float64* vec = matlab_colonop_float64(0, 1.1, 3, &n);;
 		is_int( 3, n, "0:1.1:3 is of length 3");
+		free(vec);
 	}
 	{/* 1 tests */
 		size_t n;
 		float64* vec = matlab_colonop_float64(0, 1, 3.1, &n);;
 		is_int( 4, n, "0:1.1:3 is of length 4");
+		free(vec);
 	}
 	{/* 1 tests */
 		size_t n;
 		float64* vec = matlab_colonop_float64(0, 1, 3.9, &n);;
 		is_int( 4, n, "0:1.1:3 is of length 4");
+		free(vec);
 	}
 	{/* 1 tests */
 		size_t n;
 		float64* vec = matlab_colonop_float64(0, 1, 4, &n);;
 		is_int( 5, n, "0:1.1:3 is of length 5");
+		free(vec);
 	}
 }
