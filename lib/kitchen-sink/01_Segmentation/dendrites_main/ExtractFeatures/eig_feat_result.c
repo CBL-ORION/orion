1c64c630f5aba6b6296c7d78b2fdfe5196038413
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-10-17 12:09:32 -0500

    orion_eig_feat: clean up the *_free() function

--- lib/kitchen-sink/01_Segmentation/dendrites_main/ExtractFeatures/eig_feat_result.c
+++ lib/kitchen-sink/01_Segmentation/dendrites_main/ExtractFeatures/eig_feat_result.c
@@ -1,6 +1,7 @@
 #include "eig_feat_result.h"
 
 #include "util/mem.h"
+#include "simple-log/simplelog.h"
 
 orion_eig_feat_result* orion_eig_feat_result_new() {
 	orion_eig_feat_result* res;
@@ -11,8 +12,11 @@ orion_eig_feat_result* orion_eig_feat_result_new() {
 
 void orion_eig_feat_result_free(orion_eig_feat_result* e) {
 	if( e->eig_feat ) {
-		for( size_t a_idx = 0; a_idx < array_length_ndarray3(e->eig_feat); a_idx++) {
-			ndarray3_free( array_get_ndarray3(e->eig_feat, a_idx) );
+		size_t a_len = array_length_ndarray3(e->eig_feat);
+		for( size_t a_idx = 0; a_idx < a_len; a_idx++) {
+			ndarray3* n = array_get_ndarray3(e->eig_feat, a_idx);
+			/*[>DEBUG<]LOG_DEBUG("Free data from ndarray3 (%p) at (%p)", n, n->p);*/
+			ndarray3_free( n );
 		}
 		array_free_ndarray3(e->eig_feat);
 	}
