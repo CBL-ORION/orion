4dca6cec372e88d4db512fac7d008fbe7e46a5bf
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-10-18 07:35:16 -0500

    MetaImage: check that the read in volumes have the expected summation
    The values used come from simple ITK code that uses the
    `itk::StatisticsImageFilter` and its `GetSum()` method.

--- lib/t/io/mhd.c
+++ lib/t/io/mhd.c
@@ -1,12 +1,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <tap/basic.h>
+#include <tap/float.h>
 
 #include "ndarray/ndarray3.h"
 #include "io/format/mhd.h"
 
 int main(void) {
-	plan(4 + 4);
+	const float64 eps = 1e-15;
+	plan(4 + 5 + 5);
 
 	char* mhd_file = "test-data/DIADEM/NPF/NPF023/NPF023.mhd";
 
@@ -23,7 +25,7 @@ int main(void) {
 	}
 
 	{
-		/* 4 tests */
+		/* 5 tests */
 		ndarray3* vol = orion_read_mhd( mhd_file );
 		ok( NULL != vol, "reading the MetaInfo file gives a non-NULL ndarray");
 
@@ -32,9 +34,33 @@ int main(void) {
 		is_int( 1024, vol->sz[1], "2nd dim is correct size");
 		is_int(   57, vol->sz[2], "3rd dim is correct size");
 
+		/* find the sum to expect by reading in the data:
+		 *
+		 * ../test/sum-of-an-metainfo-volume/SumMetaInfoVol test-data/DIADEM/NPF/NPF023/NPF023
+		 */
+		is_double(   11952376197.0000L, ndarray3_sum_over_all_float64(vol), eps, "sum of the volume is as expected");
+
 		ndarray3_free(vol);
 	}
 
+	{
+		/* 5 tests */
+		ndarray3* vol = orion_read_mhd( "test-data/ITK/HeadMRVolume/HeadMRVolume.mhd" );
+		ok( NULL != vol, "reading the MetaInfo file gives a non-NULL ndarray");
+
+		/* check the size */
+		is_int( 48, vol->sz[0], "1st dim is correct size");
+		is_int( 62, vol->sz[1], "2nd dim is correct size");
+		is_int( 42, vol->sz[2], "3rd dim is correct size");
+
+		/* find the sum to expect by reading in the data:
+		 *
+		 * ../test/sum-of-an-metainfo-volume/SumMetaInfoVol test-data/ITK/HeadMRVolume/HeadMRVolume
+		 */
+		is_double(   3058332.0000L, ndarray3_sum_over_all_float64(vol), 0, "sum of the volume is as expected");
+
+		ndarray3_free(vol);
+	}
 
 
 
