50065944ca1ccfb28de5af26a6f315e6a7e84771
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-10-28 05:10:50 -0500

    wip: implement skeleton of feature extraction handling

---
+++ lib/kitchen-sink/01_Segmentation/dendrites_main/ExtractFeatures/computeFeatures.h
@@ -0,0 +1,25 @@
+#ifndef ORION_COMPUTE_FEATURES_H
+#define ORION_COMPUTE_FEATURES_H 1
+
+/* system headers */
+#include <stdlib.h>
+#include <stdbool.h>
+
+/* local headers */
+#include "ndarray/ndarray3.h"
+#include "container/vector.h"
+#include "param/segmentation.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/* Function prototypes */
+extern vector_float* orion_computeFeatures( orion_segmentation_param* param, ndarray3* vol );
+
+#ifdef __cplusplus
+};
+#endif /* __cplusplus */
+
+#endif /* ORION_COMPUTE_FEATURES_H */
