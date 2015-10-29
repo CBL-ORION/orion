ea2947913b56c490012b4c13e9ec1126a8913217
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-10-29 03:05:00 -0500

    ORION3_Dendrites: set up tests

--- lib/kitchen-sink/01_Segmentation/dendrites_main/ORION3_Dendrites.c
+++ lib/kitchen-sink/01_Segmentation/dendrites_main/ORION3_Dendrites.c
@@ -2,11 +2,11 @@
  *
  * Refactor: ORION3_Dendrites
  */
-
-
-#include "kitchen-sink/01_Segmentation/dendrites_main/DetectTrainingSet/readNegativeSamples.h"
+#include "kitchen-sink/01_Segmentation/dendrites_main/ORION3_Dendrites.h"
 
 #include "kitchen-sink/01_Segmentation/dendrites_main/settingDefaultParameters.h"
+#include "kitchen-sink/01_Segmentation/dendrites_main/DetectTrainingSet/readNegativeSamples.h"
+#include "kitchen-sink/01_Segmentation/dendrites_main/ExtractFeatures/getFeatures.h"
 
 /*
  * Segmentation process which computes the distribution of the eigenvalues of
@@ -20,18 +20,27 @@ void orion_ORION3_Dendrites(orion_segmentation_param* param, ndarray3* vol) {
 	/* set the default parameters for the algorithm if none are set */
 	orion_settingDefaultParameters( param );
 
-	/*DEBUG*/orion_segmentation_param_dump( param );
+	/*[>DEBUG<]orion_segmentation_param_dump( param );*/
 
-	/*
-	 * MAIN FUNCTION TO OBTAIN THE TRAINING SET OF BACKGROUND VOXELS
-	 *
-	 */
-	/*
-	 * STEP 1
-	 * reading background samples using the Laplacian with the ISOTROPIC LOW
-	 * PASS FILTER
+        /*********************************************************************/
+        /* MAIN FUNCTION TO OBTAIN THE TRAINING SET OF BACKGROUND VOXELS     */
+        /*                                                                   */
+        /* Run the code to compute the response to the discriminant function */
+        /*********************************************************************/
+
+	/* STEP 1
+	 * ======
+	 * reading background samples using the Laplacian with
+	 * the **isotropic low pass filter**
 	 */
 	orion_readNegativeSamples(param, vol);
+
+	/* STEP 2
+	 * ======
+	 *
+	 * Compute the feature vector
+	 */
+	vector_float* F = orion_getFeatures(param, vol);
 
 	WARN_UNIMPLEMENTED;
 }
