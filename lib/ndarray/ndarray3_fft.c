c9c0fce9ff08c84d35d13db02d52946b28a3ffdf
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-10-29 03:10:02 -0500

    ndarray3_fft: function prototypes for forward and reverse FFT

---
+++ lib/ndarray/ndarray3_fft.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+ndarray3* ndarray3_fftn( ndarray3* n ) {
+	WARN_UNIMPLEMENTED;
+}
+
+ndarray3* ndarray3_ifftn_symmetric( ndarray3* n ) {
+	WARN_UNIMPLEMENTED;
+}
