cf6132b27cb23c8b6a27add7262f190c47bf25a3
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-10-31 12:56:08 -0500

    ndarray3_fft: add `ndarray3_complex_printf` function for debugging

--- lib/ndarray/ndarray3_complex.c
+++ lib/ndarray/ndarray3_complex.c
@@ -30,3 +30,26 @@ void ndarray3_complex_free( ndarray3_complex* n ) {
 	if( ! n->wrap ) free( n->p );
 	free( n );
 }
+
+
+void ndarray3_complex_printf(ndarray3_complex* n,
+		const char* variable_name,
+		const char* real_format,
+		char* imag_format) {
+	for( int i = 0; i < n->sz[0]; i++ ) {
+		printf("%s[%d][:][:] = {\n", variable_name, i);
+		for( int j = 0; j < n->sz[1]; j++ ) {
+			printf("\t");
+			for( int k = 0; k < n->sz[2]; k++ ) {
+				complex_pixel_type z =
+					ndarray3_complex_get( n, i,j,k);
+				printf(real_format, creal(z) );
+				printf(" %c ", cimag(z) >= 0 ? '+' : '-');
+				printf(imag_format, cimag(z) );
+				printf(" ");
+			}
+			printf("\n");
+		}
+		printf("}\n");
+	}
+}
