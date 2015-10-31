77a719a5be20af3a073a55d16f6cf31135537b51
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-10-31 05:58:02 -0500

    ndarray3_fft: implement forward and reverse real FFT
    This passes `ndarray3`/`ndarray3_complex` to Kiss-FFT.

--- lib/ndarray/ndarray3_fft.h
+++ lib/ndarray/ndarray3_fft.h
@@ -10,17 +10,15 @@
 
 /* local headers */
 #include "ndarray/ndarray3.h"
-
-/* structs, enums */
-typedef void ndarray3_complex; /* TODO remove this later */
+#include "ndarray/ndarray3_complex.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
 
 /* Function prototypes */
-extern ndarray3_complex* ndarray3_fftn( ndarray3* n );
-extern ndarray3* ndarray3_ifftn_symmetric( ndarray3* n );
+extern ndarray3_complex* ndarray3_fftn_r2c( ndarray3* n );
+extern ndarray3* ndarray3_ifftn_c2r( ndarray3_complex* n );
 
 
 #ifdef __cplusplus
