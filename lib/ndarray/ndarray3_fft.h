868f26ef097157b5b29f36d6ee286e54ea55da40
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-11-01 00:22:11 -0500

    ndarray3_fft: add documentation on conjugate symmetry

--- lib/ndarray/ndarray3_fft.h
+++ lib/ndarray/ndarray3_fft.h
@@ -17,6 +17,25 @@ extern "C" {
 #endif /* __cplusplus */
 
 /* Function prototypes */
+
+/* These functions calculate the DFT on real n-d data.
+ *
+ * The Fourier transform of real data is conjugate symmetric and so taking the
+ * inverse transform of that data is real-valued.
+ *
+ * So, given an real n-d input `x`, we have
+ *
+ *     ndarray3_ifftn_c2r( ndarray3_fftn_r2c( x ) ) == x
+ *
+ * modulo numerical errors. 
+ *
+ * See also:
+ *    Kiss-FFT:
+ *        kiss_fftndr, kiss_fftndri
+ *
+ *    MATLAB:
+ *        ifftn( x, 'symmetric' );
+ */
 extern ndarray3_complex* ndarray3_fftn_r2c( ndarray3* n );
 extern ndarray3* ndarray3_ifftn_c2r( ndarray3_complex* n );
 
