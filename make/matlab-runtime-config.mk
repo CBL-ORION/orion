0a9c91f9dc2815bd6448888f206ca95903f6cdd3
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-12-27 18:47:08 -0600

    MATLAB Compiler Runtime is not needed when not using liborion3mat

--- make/matlab-runtime-config.mk
+++ make/matlab-runtime-config.mk
@@ -1,3 +1,5 @@
+ifdef FEAT_LIBORION3MAT
+
 # MATLAB install R2013a (v8.1)
 # The value of ${MATLAB} can be obtained by running:
 #     mex -v 2>/dev/null | grep "MATLAB *="
@@ -29,3 +31,5 @@ MCR_LDLIBS   := -lmwmclmcrrt -lm
 MATLAB_LD_LIBRARY_PATH := ${MATLAB}/bin/glnxa64${PATHSEP}${MATLAB}/runtime/glnxa64
 MCR_LD_LIBRARY_PATH := ${MCR}/runtime/glnxa64${PATHSEP}${MCR}/bin/glnxa64${PATHSEP}${MCR}/sys/os/glnxa64${PATHSEP}${MCR}/sys/java/jre/glnxa64/jre/lib/amd64/native_threads${PATHSEP}${MCR}/sys/java/jre/glnxa64/jre/lib/amd64/server${PATHSEP}${MCR}/sys/java/jre/glnxa64/jre/lib/amd64
 XAPPLRESDIR := ${MCR}/X11/app-defaults
+
+endif # FEAT_LIBORION3MAT
