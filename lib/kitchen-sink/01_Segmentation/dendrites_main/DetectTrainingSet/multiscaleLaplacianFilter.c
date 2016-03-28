cd7ace1edd041e983be1fa20dec954122ce3dfb9
Zakariyya Mughal <zaki.mughal@gmail.com>
2016-03-27 23:10:40 -0500

    Finalise the multiscale Laplacian filter

--- lib/kitchen-sink/01_Segmentation/dendrites_main/DetectTrainingSet/multiscaleLaplacianFilter.c
+++ lib/kitchen-sink/01_Segmentation/dendrites_main/DetectTrainingSet/multiscaleLaplacianFilter.c
@@ -6,6 +6,7 @@
 #include "kitchen-sink/01_Segmentation/dendrites_main/DetectTrainingSet/multiscaleLaplacianFilter.h"
 
 #include "util/util.h"
+#include "simple-log/simplelog.h"
 #include "kitchen-sink/01_Segmentation/dendrites_main/DetectTrainingSet/IsotropicFilter/Makefilter.h"
 #include "ndarray/ndarray3_fft.h"
 
@@ -54,36 +55,7 @@ orion_multiscale_laplacian_output* orion_multiscaleLaplacianFilter(
 
 	/* Fourier transform the input image */
 	ndarray3_complex* v_fft = ndarray3_fftn_r2c(input_volume);
-	/*[>DEBUG<]ndarray3_complex_printf(v_fft, "v", "%8.3f", "%8.3f");*/
-	/*[>REMOVE DEBUG<]{
-		ndarray3* back =
-			ndarray3_ifftn_c2r( v_fft );
 
-		complex double ss = 0;
-		float64 sss = 0;
-		float64 inf_norm = -INFINITY;
-		for( int i = 0; i < ndarray3_complex_elems(v_fft); i++ ) {
-			sss += input_volume->p[i];
-			ss += v_fft->p[i];
-
-			float64 d = fabs(input_volume->p[i]
-				- back->p[i]);
-			if( d > inf_norm ) {
-				[> find max <]
-				inf_norm = d;
-			}
-		}
-		printf("sss %f; sum = %g + %gi\n", sss, creal(ss), cimag(ss));
-		complex_pixel_type zz000 = ndarray3_complex_get(v_fft, 0,0,0);
-		printf("zz(0,0,0) %f + %fi\n",  creal(zz000), cimag(zz000));
-		complex_pixel_type zz111 = ndarray3_complex_get(v_fft, 1,1,1);
-		printf("zz(1,1,1) %f + %fi\n",  creal(zz111), cimag(zz111));
-		printf("norm_∞: %f\n", inf_norm);
-
-
-		ndarray3_free(back);
-
-	}*/
 	output->laplacian
 		= ndarray3_new_with_size_from_ndarray3(input_volume);
 	output->max_response_at_scale_idx
@@ -106,11 +78,11 @@ orion_multiscale_laplacian_output* orion_multiscaleLaplacianFilter(
 	for( int lap_idx = 0; lap_idx < lap_scale_len; lap_idx++ ) {
 		/* compute the Laplacian for each scale */
 		float laplacian_scale_factor = array_get_float( laplacian_scales, lap_idx );
+		LOG_INFO("Computing Laplacian using scale: %f", laplacian_scale_factor);
 
 		float64 norm_const = _orion_ConstantToNormalizeFilter(laplacian_scale_factor);
 		ndarray3* filt = orion_Makefilter( nx,ny,nz,
 			n, laplacian_scale_factor, orion_Makefilter_FLAG_A);
-
 
 		NDARRAY3_LOOP_OVER_START( filt, i,j,k) {
 			/* normalise the generated Laplacian filter
@@ -130,8 +102,8 @@ orion_multiscale_laplacian_output* orion_multiscaleLaplacianFilter(
 			 * NOTE: This is a complex multiplied by a scalar.
 			 */
 			ndarray3_complex_set(cur_scale_laplacian, i,j,k,
-				  ndarray3_complex_get(v_fft, i,j,k)
-				* ndarray3_get(filt,         i,j,k)
+				  ndarray3_complex_get(v_fft,     i,j,k)
+				* ndarray3_get(filt,              i,j,k)
 			);
 		} NDARRAY3_LOOP_OVER_END;
 
@@ -162,7 +134,7 @@ orion_multiscale_laplacian_output* orion_multiscaleLaplacianFilter(
 				/* Multiscale approach */
 				NDARRAY3_LOOP_OVER_START( cur_scale_filt_vol, i,j,k) {
 					pixel_type cur = ndarray3_get(cur_scale_filt_vol, i,j,k);
-					pixel_type lap = ndarray3_get(output->laplacian, i,j,k);
+					pixel_type lap = ndarray3_get(output->laplacian,  i,j,k);
 					if(   fabs(cur) >= fabs(lap) ) {
 						/* the current scale is recorded as the maximum at this point */
 						ndarray3_set(output->laplacian, i,j,k, lap);
@@ -171,6 +143,9 @@ orion_multiscale_laplacian_output* orion_multiscaleLaplacianFilter(
 				} NDARRAY3_LOOP_OVER_END;
 			} else {
 				/* ISBI 2014 */
+				/* Check if the values themselves are larger
+				 * than the current maximum
+				 */
 				NDARRAY3_LOOP_OVER_START( cur_scale_filt_vol, i,j,k) {
 					pixel_type cur = ndarray3_get(cur_scale_filt_vol, i,j,k);
 					pixel_type lap = ndarray3_get(output->laplacian, i,j,k);
