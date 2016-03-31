b58fe55916dd6cbb668545204dfd7ac135e5672e
Zakariyya Mughal <zaki.mughal@gmail.com>
2016-03-31 11:58:23 -0500

    Helper function to compute features

--- lib/kitchen-sink/01_Segmentation/dendrites_main/ExtractFeatures/getFeatures.c
+++ lib/kitchen-sink/01_Segmentation/dendrites_main/ExtractFeatures/getFeatures.c
@@ -6,11 +6,13 @@
 #include "kitchen-sink/01_Segmentation/dendrites_main/ExtractFeatures/getFeatures.h"
 
 #include "simple-log/simplelog.h"
-#include "kitchen-sink/01_Segmentation/dendrites_main/ExtractFeatures/computeFeatures.h"
 
-vector_float* orion_getFeatures( orion_segmentation_param* param, ndarray3* vol ) {
+orion_features* orion_getFeatures(
+		orion_segmentation_param* param,
+		ndarray3* vol,
+		orion_multiscale_laplacian_output* laplacian_result ) {
 	LOG_INFO("Computing Eigenvalues of Hessian Matrix...");
-	vector_float* feat = orion_computeFeatures(param, vol);
+	orion_features* feat = orion_computeFeatures(param, vol, laplacian_result );
 	LOG_INFO("Done computing eigenvalues.");
 	return feat;
 }
