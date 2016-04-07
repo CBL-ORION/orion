2291b9fe2aa77f743f368723e19f8187df8855d8
Zakariyya Mughal <zaki.mughal@gmail.com>
2016-04-07 16:19:41 -0500

    Refactor the KISS99 RNG code
    Wrap all the seed values inside a struct

--- lib/numeric/kiss99_rng.c
+++ lib/numeric/kiss99_rng.c
@@ -1,40 +1,74 @@
 #include <stdio.h>
-#define znew (z=36969*(z&65535)+(z>>16))
-#define wnew (w=18000*(w&65535)+(w>>16))
-#define MWC ((znew<<16)+wnew )
-#define SHR3 (jsr^=(jsr<<17), jsr^=(jsr>>13), jsr^=(jsr<<5))
-#define CONG (jcong=69069*jcong+1234567)
-#define FIB ((b=a+b),(a=b-a))
-#define KISS ((MWC^CONG)+SHR3)
-#define LFIB4 (c++,t[c]=t[c]+t[UC(c+58)]+t[UC(c+119)]+t[UC(c+178)])
-#define SWB (c++,bro=(x<y),t[c]=(x=t[UC(c+34)])-(y=t[UC(c+19)]+bro))
-#define UNI (KISS*2.328306e-10)
-#define VNI ((long) KISS)*4.656613e-10
-#define UC (unsigned char) /*a cast operation*/
-typedef unsigned long UL;
+#include <stdint.h>
+
+/*
+ * George Marsaglia's KISS99 PRNG.
+ *
+ * Taken from Usenet posting from 1999-01-21 <http://www.cse.yorku.ca/~oz/marsaglia-rng.html>..
+ *
+ * Modified to keep state in struct.
+ */
+
+#define kiss99_rng_znew(state) (state->z=36969*(state->z&65535)+(state->z>>16))
+#define kiss99_rng_wnew(state) (state->w=18000*(state->w&65535)+(state->w>>16))
+#define kiss99_rng_MWC(state) ((kiss99_rng_znew(state)<<16)+kiss99_rng_wnew(state) )
+#define kiss99_rng_SHR3(state) (state->jsr^=(state->jsr<<17), state->jsr^=(state->jsr>>13), state->jsr^=(state->jsr<<5))
+#define kiss99_rng_CONG(state) (state->jcong=69069*state->jcong+1234567)
+#define kiss99_rng_FIB(state) ((state->b=state->a+state->b),(state->a=state->b-state->a))
+#define kiss99_rng_KISS(state) ((kiss99_rng_MWC(state)^kiss99_rng_CONG(state))+kiss99_rng_SHR3(state))
+#define kiss99_rng_LFIB4(state) (state->c++,state->t[state->c]=state->t[state->c]+state->t[kiss99_rng_UC(state->c+58)]+state->t[kiss99_rng_UC(state->c+119)]+state->t[kiss99_rng_UC(state->c+178)])
+#define kiss99_rng_SWB(state) (state->c++,state->bro=(state->x<state->y),state->t[state->c]=(state->x=state->t[kiss99_rng_UC(state->c+34)])-(state->y=state->t[kiss99_rng_UC(state->c+19)]+state->bro))
+#define kiss99_rng_UNI(state) (kiss99_rng_KISS(state)*2.328306e-10)
+#define kiss99_rng_VNI(state) ((long) kiss99_rng_KISS(state))*4.656613e-10
+#define kiss99_rng_UC (unsigned char) /*a cast operation*/
 /* Global static variables: */
-static UL z=362436069, w=521288629, jsr=123456789, jcong=380116160;
-static UL a=224466889, b=7584631, t[256];
+typedef struct {
+	/* seed values */
+	uint32_t z/*=362436069*/;
+	uint32_t w/*=521288629*/;
+	uint32_t jsr/*=123456789*/;
+	uint32_t jcong/*=380116160*/;
+	uint32_t a/*=224466889*/;
+	uint32_t b/*=7584631*/;
+	/* state table */
+	uint32_t t[256];
+
+	/* non-seed state: init to 0 */
+	uint32_t x/*=0*/;
+	uint32_t y/*=0*/;
+
+	/* temporary storage */
+	uint32_t bro;
+
+	/* counter: init to zero */
+	unsigned char c/*=0*/;
+} kiss99_rng_state;
+
 /* Use random seeds to reset z,w,jsr,jcong,a,b, and the table
-t[256]*/
-static UL x=0,y=0,bro; static unsigned char c=0;
+*/
 /* Example procedure to set the table, using KISS: */
-void settable(UL i1,UL i2,UL i3,UL i4,UL i5, UL i6)
-{ int i; z=i1;w=i2,jsr=i3; jcong=i4; a=i5; b=i6;
-for(i=0;i<256;i=i+1) t[i]=KISS;
+void kiss99_rng_set_table(kiss99_rng_state* state, uint32_t i1,uint32_t i2,uint32_t i3,uint32_t i4,uint32_t i5,uint32_t i6) {
+	int i;
+
+	/* init state */
+	state->x = 0; state->y = 0; state->c = 0;
+
+	state->z=i1;state->w=i2,state->jsr=i3; state->jcong=i4; state->a=i5; state->b=i6;
+	for(i=0;i<256;i=i+1) state->t[i]=kiss99_rng_KISS(state);
 }
-/* This is a test main program. It should compile and print 7
-0's. */
-int main(void){
-int i; UL k;
-settable(12345,65435,34221,12345,9983651,95746118);
-for(i=1;i<1000001;i++){k=LFIB4;} printf("%u\n", k-1064612766U);
-for(i=1;i<1000001;i++){k=SWB ;} printf("%u\n", k- 627749721U);
-for(i=1;i<1000001;i++){k=KISS ;} printf("%u\n", k-1372460312U);
-for(i=1;i<1000001;i++){k=CONG ;} printf("%u\n", k-1529210297U);
-for(i=1;i<1000001;i++){k=SHR3 ;} printf("%u\n", k-2642725982U);
-for(i=1;i<1000001;i++){k=MWC ;} printf("%u\n", k- 904977562U);
-for(i=1;i<1000001;i++){k=FIB ;} printf("%u\n", k-3519793928U);
+/* This is a test main program. It should compile and print 7 0's. */
+int main(void) {
+	int i; uint32_t k;
+	kiss99_rng_state s;
+	kiss99_rng_state* state = &s;
+	kiss99_rng_set_table(state, 12345,65435,34221,12345,9983651,95746118);
+	for(i=1;i<1000001;i++){k=kiss99_rng_LFIB4(state);} printf("%u\n", k-1064612766U);
+	for(i=1;i<1000001;i++){k=kiss99_rng_SWB(state) ;} printf("%u\n", k- 627749721U);
+	for(i=1;i<1000001;i++){k=kiss99_rng_KISS(state) ;} printf("%u\n", k-1372460312U);
+	for(i=1;i<1000001;i++){k=kiss99_rng_CONG(state) ;} printf("%u\n", k-1529210297U);
+	for(i=1;i<1000001;i++){k=kiss99_rng_SHR3(state) ;} printf("%u\n", k-2642725982U);
+	for(i=1;i<1000001;i++){k=kiss99_rng_MWC(state) ;} printf("%u\n", k- 904977562U);
+	for(i=1;i<1000001;i++){k=kiss99_rng_FIB(state) ;} printf("%u\n", k-3519793928U);
 }
 /*-----------------------------------------------------
 Write your own calling program and try one or more of
